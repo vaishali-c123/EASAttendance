988a236dccf2fe1e12b0788d34d249a0bab559b5
Prayag Dubey <prayagdubey1010@gmail.com>
2022-05-20 17:11:21 +0530

    first commit

---
+++ eams_attendance_api/Model/Attendance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeAttendanceSystem.Models
+{
+    public class Attendance
+    {
+        [Key]
+        public string AttendanceId { get; set; }
+
+        public string AttendanceStatus { get; set; }
+
+        public string AttendanceDate { get; set; }
+
+       
+    }
+}
